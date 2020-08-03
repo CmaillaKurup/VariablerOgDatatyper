c17c0807b414d99e768c7549f375ab2079a40336
cami670v <cami670v@ZBC-AA-UBUY12>
2020-08-03 13:46:12 +0200

    Add project files.

---
+++ VariablerOgDatatyper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariablerOgDatatyper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 16;
+            double b = 5.1;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
